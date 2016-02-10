2fcc481ed0f04cc9bc6c883a6a0455fc79dbb5e9
grout_r <grout_r>
2016-02-10 16:23:52 +0100

    form remonte

--- routes/register.js
+++ routes/register.js
@@ -3,9 +3,25 @@
  */
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
-router.get('/', function(req, res, next) {
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+router.get('/', function(req, res, next)
+{
     res.render('register.hbs');
 });
 
+router.post('/submit', urlencodedParser, function (req, res)
+{
+    // Prepare output in JSON format
+    var response = {
+        username:req.body.username,
+        password:req.body.password,
+        country:req.body.country
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
 module.exports = router;
